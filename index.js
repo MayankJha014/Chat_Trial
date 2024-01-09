3a7e92d87e86f99bec3f77593b2aa6db8f191e0f
Mayank Jha <132004139+MayankJha014@users.noreply.github.com>
2024-01-09 08:58:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const messageRouter = require("./routes/message");
 const app = express();
 const { Server } = require("socket.io");
 const http = require("http");
+require("dotenv").config();
 
 // const cors = require("./config/cors");
 
 //Done
 
 const PORT = 4000;
-const DB =
-  "mongodb+srv://mayankjha014:1234@cluster0.irjg6zb.mongodb.net/Social_Message?retryWrites=true&w=majority";
+const DB = process.env.DB;
 app.use(
   cors({
     origin: "https://chat-messanger-front.vercel.app",
